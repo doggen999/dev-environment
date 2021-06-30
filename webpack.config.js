1401247be0fa637ccca6a228b9bd1367517d576b
Natan Williams <natan.williams123@gmail.com>
2021-06-30 16:41:03 +0200

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,18 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  module: { rules: [{ test: /\.(js|jsx)$/, use: { loader: "babel-loader" } }] },
   output: {
-    clean: true,
-    filename: "bundle.js",
     path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: { loader: "babel-loader" },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: path.join(__dirname, "src/index.html") }),
