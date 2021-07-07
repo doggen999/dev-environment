d5e46504c1d0b80b43111ba2048633932672cf80
Natan Williams <natan.williams123@gmail.com>
2021-07-07 21:08:52 +0200

    Add global styling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import React from "react";
+
+import styles from './app.scss'
+
 import Component from "./components/Component";
 
-export default () => <Component />;
+export default () => (<div className={styles.app}>
+    <Component />
+</div>);
