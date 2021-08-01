8e164aad69db09ef027aa7c564372181fbc47f35
Natan Williams <natan.williams123@gmail.com>
2021-08-01 23:59:35 +0200

    Add Fakerjs for mocking support

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from "react";
+import faker from "faker";
 import styles from "./app.scss";
 
 import Component from "./components/Component";
 
 export default () => (
   <div className={styles.app}>
-    <Component heading={"Heading"} />
+    <Component heading={faker.company.catchPhrase()} />
   </div>
 );
