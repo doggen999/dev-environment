c5d252aa88fb75b74c433fb45587c495cb9c3364
Natan Williams (ext) <natan.williams.external@eon.com>
2022-10-30 14:54:38 +0100

    Update Jest with basic configuration for React and snapshot testing

---
+++ src/examples/components/link/Link.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const STATUS = {
+  HOVERED: 'hovered',
+  NORMAL: 'normal',
+};
+
+// export default function Link({}) {}
+
+export default ({ page, children }) => {
+  const [status, setStatus] = useState(STATUS.NORMAL);
+
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
+
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
+
+  return (
+    <a
+      className={status}
+      href={page || '#'}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {children}
+    </a>
+  );
+};
