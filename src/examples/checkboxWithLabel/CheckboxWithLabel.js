30df3370b7b0016f410b497f72adcb26272d86ae
Natan Williams (ext) <natan.williams.external@eon.com>
2022-10-30 15:21:36 +0100

    Add DOM testing with react-testing-library

---
+++ src/examples/checkboxWithLabel/CheckboxWithLabel.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export default ({ labelOn, labelOff }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      {isChecked ? labelOn : labelOff}
+    </label>
+  );
+};
